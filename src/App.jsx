530eaaa84de76b700ed84caa47e0eeb66722b62b
konjuable <juha.konttinen@bc.fi>
2023-09-21 12:09:39 +0300

    04 Hello-komponentti.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-const App = () => {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log(now, a + b);
+const Hello = () => {
+  return (
+    <section>
+      <p>Hello World</p>
+    </section>
+  );
+};
 
+const App = () => {
   return (
     <>
-      <p>Hello World, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </>
   );
 };
