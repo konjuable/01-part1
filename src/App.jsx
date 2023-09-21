b63a23343bac4b254829a412e9a0fb541a46105b
konjuable <juha.konttinen@bc.fi>
2023-09-21 13:02:45 +0300

    07 Hello-, Footer ja Friends-komponentit ja datan välittäminen niihin propsien avulla.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,17 @@ const Hello = (props) => {
   );
 };
 
+const Friends = (props) => {
+  console.log(props);
+  return (
+    <section>
+      <p>
+        My dear friend {props.nimi} is {props.ika} years old.
+      </p>
+    </section>
+  );
+};
+
 const Footer = (props) => {
   console.log(props);
   return (
@@ -23,12 +34,18 @@ const App = () => {
   const author = "Juha Konttinen";
   const nimi = "Pekka";
   const ika = 37;
+  const friends = [
+    { name: "Leevi", age: 4 },
+    { name: "Venla", age: 10 },
+  ];
   return (
     <>
       <h1>Greetings</h1>
       <Hello name="Maija" age={26 + 10} />
       <Hello name={nimi} age={ika} />
       <Hello name="Matti" age="38" />
+      <Friends nimi={friends[0].name} ika={friends[0].age} />
+      <Friends nimi={friends[1].name} ika={friends[1].age} />
       <Footer nimi={author} />
     </>
   );
